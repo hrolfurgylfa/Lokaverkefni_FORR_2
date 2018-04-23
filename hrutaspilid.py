91442dfa40a49845b42d10dea0de0bd4548c0579
MikaelAndriIngason <33830730+MikaelAndriIngason@users.noreply.github.com>
2018-04-23 11:22:27 +0000

    Add files via upload

---
+++ hrutaspilid.py
@@ -0,0 +1,24 @@
+'''
+Mikael og Hrólfur
+4/23/2018
+
+Lokaverkefni - Hrútaspilið
+'''
+
+class Spil():
+
+    fj_hruta = 0
+
+    def __init__(self, thyngd, mjolk, ull, fjAfkvaema, laeri, frjosemi, thykkiBaks, malir):
+        self.thyngd = thyngd
+        self.mjolk = mjolk
+        self.ull = ull
+        self.fjAfkvæma = fjAfkvæma
+        self.laeri = laeri
+        self.frjosemi = frjosemi
+        self.thykkiBaks = thykkiBaks
+        self.malir = malir
+
+
+
+        
